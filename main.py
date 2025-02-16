08a6932c78152337cb3c70f2731a1c25027f7f1f
chengdsyu <adrianneantoinette@gmail.com>
2025-02-16 21:43:05 +1100

    added constants, circleshape and player

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
-  pygame.get_init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
-  while True:
-     for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-         return
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-     screen.fill("black")
-     pygame.display.flip()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
